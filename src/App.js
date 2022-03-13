093c5a7bb5a1492fac4ef1b4ec6844ad17c50109
saksham37 <59642935+saksham37@users.noreply.github.com>
2022-03-13 11:43:35 +0530

    commit 1

--- src/App.js
+++ src/App.js
@@ -3,11 +3,63 @@ import "./styles.css";
 import TodoItem from "./TodoItem.js";
 
 class App extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      todos: [
+        {
+          id: 1,
+          text: "My task 1",
+          completed: true
+        },
+        {
+          id: 2,
+          text: "My task 2",
+          completed: true
+        },
+        {
+          id: 3,
+          text: "My task 3",
+          completed: true
+        },
+        {
+          id: 4,
+          text: "My task 4",
+          completed: true
+        },
+        {
+          id: 5,
+          text: "My task 5",
+          completed: true
+        }
+      ]
+    };
+  }
+  handleChange = (item) => {
+    console.log("handleChange called");
+    const todoList = this.state.todos;
+    let index = todoList.indexOf(item);
+    todoList[index].completed = !todoList[index].completed;
+    this.setState({
+      todos: todoList
+    });
+  };
   render() {
+    const todoList = this.state.todos;
+    // const handleChange = this.handleChange;
+    // console.log(handleChange);
     return (
       <div className="todo-list">
         <h1>My Daily Goals</h1>
-        <TodoItem />
+        {todoList.map((item) => {
+          return (
+            <TodoItem
+              item={item}
+              onHandleChange={this.handleChange}
+              key={item.id}
+            />
+          );
+        })}
       </div>
     );
   }
--- src/TodoItem.js
+++ src/TodoItem.js
@@ -1,32 +1,21 @@
 import React from "react";
 
-class TodoItem extends React.Component {
-  constructor() {
-    super();
-    this.state = {
-      id: 1,
-      text: "My Task",
-      completed: true
-    };
-    this.handleChange = this.handleChange.bind(this);
-  }
-  handleChange() {
-    this.setState((prevState) => {
-      return { completed: !prevState.completed };
-    });
-  }
-  render() {
-    return (
-      <div className="todo-item">
-        <input
-          type="checkbox"
-          checked={this.state.completed}
-          onChange={this.handleChange}
-        />
-        <label> {this.state.text} </label>
-      </div>
-    );
-  }
-}
+const TodoItem = (props) => {
+  const { item, onhandleChange } = props;
+  console.log(onhandleChange);
+
+  return (
+    <div className="todo-item">
+      <input
+        type="checkbox"
+        checked={item.completed}
+        onChange={() => {
+          onhandleChange(item);
+        }}
+      />
+      <label> {item.text} </label>
+    </div>
+  );
+};
 
 export default TodoItem;
